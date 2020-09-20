a6114ac909860acf8048e8d6c2467eaec5870134
Danil <smyshlyaevar@gmail.com>
2020-09-20 21:05:10 +0300

    changes applied

--- function.js
+++ function.js
@@ -1,6 +1,6 @@
 window.onload = function(){
-	window.setInterval(function(){
-	var time = new Date();
-	document.getElementById("clock").innerHTML = time.toLocaleTimeString();
-	}, 1000);
+  window.setInterval(function(){
+    var time = new Date();
+    document.getElementById("clock").innerHTML = time.toLocaleTimeString();
+  }, 1000);
 }
